e4219bf352b9423ddacf3330a81af319694f6f7d
alvachon <95185462+alvachon@users.noreply.github.com>
2022-06-25 19:44:25 -0400

    Add files via upload

---
+++ c00_ex02_ft_print_reverse_alphabet/ft_print_reverse_alphabet.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvachon <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 09:56:38 by alvachon          #+#    #+#             */
+/*   Updated: 2022/03/09 16:41:25 by alvachon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		ft_putchar(c);
+		c--;
+	}
+}
---
+++ c00_ex02_ft_print_reverse_alphabet/main_print_reverse_alphabet.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_print_reverse_alphabet.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvachon <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 09:59:53 by alvachon          #+#    #+#             */
+/*   Updated: 2022/02/22 10:06:48 by alvachon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
