fc371a040019e8060f5524eb46dbd96c51d064de
alvachon <alvachon93@pm.me>
2022-12-04 19:30:11 -0500

    Cleanup

--- c09_ex00_libft_creator/libft_creator.sh
+++
@@ -1,4 +0,0 @@
-echo "void	ft_putchar(char c);\nvoid	ft_swap(int *a, int *b);\nvoid	ft_putstr(char *str);\nint	ft_strlen(char *str);\nint	ft_strcmp(char *s1, char *s2);\n" > libft.a
-gcc -Wall -Werror -Wextra -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c libft.a
-ar -rc libft.a
-ranlib libft.a
